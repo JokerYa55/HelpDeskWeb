b86fa6fdace905cae3030d7813f5bb555310b925
vasil <andr_vasil@mail.ru>
2017-07-06 22:26:08 +0300

    Рабочая версия

--- src/main/java/DAO/sprUsersDAO.java
+++ src/main/java/DAO/sprUsersDAO.java
@@ -6,6 +6,7 @@
 package DAO;
 
 import beans.TSprUsers;
+import interfaces.HibernateUtil;
 
 import interfaces.beanDAO;
 import java.sql.Connection;
@@ -14,6 +15,7 @@ import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
+import org.hibernate.Session;
 
 /**
  *
@@ -38,44 +40,40 @@ public class sprUsersDAO implements beanDAO<TSprUsers, Long> {
     @Override
     public List<TSprUsers> getList() {
         log.info("getList()");
-        List<TSprUsers> res = null;
+        //List<TSprUsers> res = null;
+//        try {
+//            res = new LinkedList<>();
+//            Statement statement = conn.createStatement();
+//            String sql = "SELECT id, f_login, f_pass, f_name FROM public.t_spr_users";
+//            ResultSet rs = statement.executeQuery(sql);
+//            while (rs.next()) {
+//                TSprUsers item = new TSprUsers();
+//                item.setId(rs.getLong("id"));
+//                item.setFName(rs.getString("f_name"));
+//                item.setFPass(rs.getString("f_pass"));
+//                item.setFLogin(rs.getString("f_login"));                
+//                res.add(item);
+//                        
+//            }
+//        } catch (Exception e) {
+//        }
+
         try {
-            res = new LinkedList<>();
-            Statement statement = conn.createStatement();
-            String sql = "SELECT id, f_login, f_pass, f_name FROM public.t_spr_users";
-            ResultSet rs = statement.executeQuery(sql);
-            while (rs.next()) {
-                TSprUsers item = new TSprUsers();
-                item.setId(rs.getLong("id"));
-                item.setFName(rs.getString("f_name"));
-                item.setFPass(rs.getString("f_pass"));
-                item.setFLogin(rs.getString("f_login"));                
-                res.add(item);
-                        
-            }
+            log.info("getItemList");
+            //System.out.println("DataSource = " + dataSource);
+            Session session1 = HibernateUtil.getSessionFactory().openSession();
+            org.hibernate.Transaction tx = session1.beginTransaction();
+            List<TSprUsers> itemList = session1.createCriteria(TSprUsers.class).list();
+            tx.commit();
+            return itemList;
         } catch (Exception e) {
+            log.warning(e.getMessage());
+            return null;
         }
-        return res;
     }
 
     @Override
     public List<TSprUsers> getList(Long startIdx, Long stopIdx) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    @Override
-    public Long addItem(TSprUsers item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean updateItem(TSprUsers item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean deleteItem(TSprUsers item) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
 }
--- src/main/java/interfaces/beanDAO.java
+++ src/main/java/interfaces/beanDAO.java
@@ -6,6 +6,7 @@
 package interfaces;
 
 import java.util.List;
+import org.hibernate.Session;
 
 /**
  *
@@ -14,10 +15,68 @@ import java.util.List;
  * @param <V> Тип уникального ключа
  */
 public interface beanDAO<T, V> {
+
     public T getItem(V id);
+
     public List<T> getList();
+
     public List<T> getList(V startIdx, V stopIdx);
-    public V addItem(T item);
-    public boolean updateItem(T item);
-    public boolean deleteItem(T item);
+
+    default public long addItem(T Item) {
+        try {
+            //Session session = HibernateUtil.getSessionFactory().openSession();
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            org.hibernate.Transaction tx = session.beginTransaction();
+            long id = (Long) session.save(Item);
+            System.out.println("id = " + id);
+            tx.commit();
+            //session.close();
+            return id;
+        } catch (Exception e) {
+            System.out.println("Ошибка : " + e.getMessage());
+            return 0;
+        }
+    }
+
+    /**
+     * 
+     * @param Item
+     * @return 
+     */
+    
+    default public boolean deleteItem(T Item) {
+        try {
+            //Session session = HibernateUtil.getSessionFactory().openSession();
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            org.hibernate.Transaction tx = session.beginTransaction();
+            session.delete(Item);
+            tx.commit();
+            //session.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка : " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * 
+     * @param Item
+     * @return 
+     */
+    default public boolean updateItem(T Item) {
+        try {
+            System.out.println("updateItem Item = " + Item);
+            //Session session = HibernateUtil.getSessionFactory().openSession();
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            org.hibernate.Transaction tx = session.beginTransaction();
+            session.saveOrUpdate(Item);
+            tx.commit();
+            //session.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ошибка : " + e.getMessage());
+            return false;
+        }
+    }
 }
